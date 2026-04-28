08848d33b3b39cfe2e47f28cbdf1e5b5c9155159
David Phal Magar <davidmagar27@gmail.com>
2026-04-28 11:50:22 +0545

    garage dashboard's responsiveness finished for mobile screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/x-icon" />
+
 
         {{-- Tailwind CSS import --}}
         <script src="https://cdn.tailwindcss.com"></script>
